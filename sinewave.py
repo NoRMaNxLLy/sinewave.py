c6c2b54f63d5de75901b79545d8e7c6d66de77e5
norman <norman@archT450>
2023-01-04 11:42:55 +0200

    refactoring

--- sinewave.py
+++ sinewave.py
@@ -4,34 +4,59 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-def read_input():
-    A = float(input('Amplitude: '))
-    f = float(input('frequency: '))
-    t_start = float(input('time domain: '))
-    t_end = float(input('time domain: '))
-    P = float(input('Phase: '))
-    return A, f, t_start, t_end, P
+waves = {}
+add_noise = False
+decompose = False
+t_start, t_end = 0, 1
 
-if len(sys.argv) == 6:
-    # return a list with integer values.
-    A, f, t_start, t_end, P = [float(i) for i in sys.argv[1:]]
-else:
-    A, f, t_start, t_end, P = read_input()
+for i in range(1,len(sys.argv)):
+    arg = sys.argv[i]
 
-T = 1 / f
-s_rate = 2 * f
-# convert the Phase shift from d to rad
-P = P * 2 * np.pi / 360
+    if arg == '--wave' or arg == '-w':
+        waves[i] = sys.argv[i+1].split()
 
-# return a x and y list of cordinates
+    elif arg == '--noise' or arg == '-n':
+        add_noise = True
+
+    elif arg == '--time' or arg == '-t':
+        t_start, t_end = [float(t) for t in sys.argv[i+1].split()]
+
+    elif arg == '--decompose' or arg == '-d':
+        decompose = True
+
+
 x = np.arange(t_start, t_end, 0.001)
-y = A * np.sin(2 * np.pi * f * x + P)
+y = 0
+
+for v in waves.values():
+    # unpack the list
+    A, f, P = [float(i) for i in v]
+
+    # convert the Phase shift from d to rad
+    P = P * 2 * np.pi / 360
+
+    # return the y values
+    y += A * np.sin(2 * np.pi * f * x + P)
+
+    if decompose:
+        i = A * np.sin(2 * np.pi * f * x + P)
+        plt.plot(x, i, label=f'A = {A} f = {f} P = {P} ')
+
+# add noise if needed
+if add_noise:
+    noise = np.random.normal(scale=0.50, size=x.size)
+    y += noise
+
+    if decompose:
+        plt.plot(x, noise, label='noise')
 
 # ploting
-plt.plot(x, y)
+plt.plot(x, y, label='total')
 plt.axhline(y=0, color='black')
 plt.xlabel('Time')
 plt.ylabel('Amplitude')
 #plt.axis([-2, 2, -5, A])
-plt.title(r'$S(t) = A \sin(2\pi ft + \O)$', loc='center')
+#plt.title(r'$S(t) = A \sin(2\pi ft + \O)$', loc='center')
+if decompose:
+    plt.legend(loc='upper right')
 plt.show()
